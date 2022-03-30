ad43bc7c9903fa000ae7ca439a7b229937678b02
bhavanichandra <bhavanichandra9@live.co.uk>
2022-03-31 00:07:57 +0530

    Structs Chapter finished

---
+++ structs/structs.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+)
+
+type User struct {
+	firstName   string
+	lastName    string
+	birthDate   string
+	createdDate time.Time
+}
+
+// outputDetails prints the user details
+func (user *User) outputDetails() {
+	fmt.Printf("My name is %v %v (born on %v)", user.firstName, user.lastName, user.birthDate)
+}
+
+// NewUser created a pointer to the User instance.
+func NewUser(fName string, lName string, bDate string) *User {
+	created := time.Now()
+
+	user := User{
+		firstName:   fName,
+		lastName:    lName,
+		birthDate:   bDate,
+		createdDate: created,
+	}
+
+	return &user
+}
+
+var reader = bufio.NewReader(os.Stdin)
+
+func main() {
+	var newUser *User
+
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	newUser = NewUser(firstName, lastName, birthdate)
+
+	// ... do something awesome with that gathered data!
+	// newUser =User{firstName, lastName, birthdate, time.Now()} // Order must be maintained
+	newUser.outputDetails()
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	userInput, _ := reader.ReadString('\n')
+
+	var cleanedInput string
+
+	if runtime.GOOS == "windows" {
+		cleanedInput = strings.Replace(userInput, "\r\n", "", -1)
+	} else {
+		cleanedInput = strings.Replace(userInput, "\n", "", -1)
+	}
+	return cleanedInput
+}
